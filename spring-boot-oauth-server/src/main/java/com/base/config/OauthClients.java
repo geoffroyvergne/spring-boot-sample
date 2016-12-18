05ba8a48053642f5ec0f7db6c98a5d2e9edb0e89
Geoffroy Vergne <geoffroy.vergne@gmail.com>
2016-12-18 22:05:35 +0100

    fixes

--- spring-boot-oauth-server/src/main/java/com/base/config/OauthClients.java
+++ spring-boot-oauth-server/src/main/java/com/base/config/OauthClients.java
@@ -1,11 +1,7 @@
 package com.base.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Component
