4fc17c8ebbf808434df4d8856202bf9a83b711da
gv <gv@macbook-pro-de-gv.home>
2018-05-25 18:32:26 +0200

    updates

--- spring-boot-sleuth/src/main/java/com/base/facade/IndexController.java
+++ spring-boot-sleuth/src/main/java/com/base/facade/IndexController.java
@@ -2,12 +2,14 @@ package com.base.facade;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +18,9 @@ import java.util.Map;
 @RequestMapping("/index")
 public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping(value="/test", produces=MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public ResponseEntity<Map<Integer, String>> test() {
@@ -35,5 +40,17 @@ public class IndexController {
 
         logger.info("/index/other");
         return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+    @RequestMapping("/call/test")
+    public String callHome() {
+        logger.info("/index/call/test");
+        return restTemplate.getForObject("http://localhost:8080/index/test", String.class);
+    }
+
+    @RequestMapping("/call/other")
+    public String callOther() {
+        logger.info("/index/call/other");
+        return restTemplate.getForObject("http://localhost:8080/index/other", String.class);
     }
 }
